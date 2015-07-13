5a1a4866e54f0bf55e2b7adea71cce1cb5438f27
chends <330360233@qq.com>
2015-07-13 15:06:37 +0800

    main create<7.13 15:06>

---
+++ hhhhhhhhhh/hhhhhhhhhh/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hhhhhhhhhh
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
